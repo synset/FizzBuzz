53ff460ae3c5c1563d3b79d1a6f6788422cbcbe5
synset <synnove.settman@yahoo.com>
2020-05-15 15:58:15 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+
+            string number = Console.ReadLine();
+            Int32.TryParse(number, out int nr);
+
+            for (int i = 0; i < nr; i++)
+            {
+                string result = "";
+                if (i % 3 == 0) result = "Fizz";          
+                if (i % 4 == 0) result = result + "Buzz";
+                if (result.Length == 0) result = i.ToString();
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
